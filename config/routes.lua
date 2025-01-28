d9013a97f00b58b1e7c3ca6607ba955d75dda362
Ngoc Do <ngocdo@maruweb.vn>
2025-01-28 12:19:52 +0700

    Fix template file in sub path problem

--- config/routes.lua
+++ config/routes.lua
@@ -13,31 +13,6 @@ local routes = {
     end,
     ["/404"] = function()
         return "404 Not Found"
-    end,
-    -- Admin routes for user
-    ["/adminuser"] = function()
-        local adminuser_controller = require("app.controllers.adminuser_controller")
-        return adminuser_controller.index()
-    end,
-    ["/adminuser/create"] = function()
-        local adminuser_controller = require("app.controllers.adminuser_controller")
-        return adminuser_controller.create()
-    end,
-    ["/adminuser/store"] = function(post_data)
-        local adminuser_controller = require("app.controllers.adminuser_controller")
-        return adminuser_controller.store(post_data)
-    end,
-    ["/adminuser/edit"] = function(post_data)
-        local adminuser_controller = require("app.controllers.adminuser_controller")
-        return adminuser_controller.edit(post_data)
-    end,
-    ["/adminuser/update"] = function(post_data)
-        local adminuser_controller = require("app.controllers.adminuser_controller")
-        return adminuser_controller.update(post_data)
-    end,
-    ["/adminuser/destroy"] = function(post_data)
-        local adminuser_controller = require("app.controllers.adminuser_controller")
-        return adminuser_controller.destroy(post_data)
     end
 }
 
